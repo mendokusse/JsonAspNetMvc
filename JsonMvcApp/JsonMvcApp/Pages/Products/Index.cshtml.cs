5f0eb59ff7e8ca34ba1870d3dec6e4b6b16b634c
mendokusse <anngnev@yandex.ru>
2026-03-30 11:48:53 +0300

    Add page model for products

---
+++ JsonMvcApp/JsonMvcApp/Pages/Products/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using JsonMvcApp.Models;
+using JsonMvcApp.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JsonMvcApp.Pages.Products
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ProductFileService productFileService;
+
+        public List<Product> Products { get; set; } = new();
+
+        public IndexModel(ProductFileService productFileService)
+        {
+            this.productFileService = productFileService;
+        }
+
+        public void OnGet()
+        {
+            Products = productFileService.GetAll();
+        }
+    }
+}
