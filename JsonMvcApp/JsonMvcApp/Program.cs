53402efcd75d76cce606d7948838b77f50bdf409
mendokusse <anngnev@yandex.ru>
2026-03-30 11:02:19 +0300

    Add product model

--- JsonMvcApp/JsonMvcApp/Program.cs
+++ JsonMvcApp/JsonMvcApp/Program.cs
@@ -1,7 +1,11 @@
+using JsonMvcApp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddScoped<ProductFileService>();
 
 var app = builder.Build();
 
---
+++ JsonMvcApp/JsonMvcApp/Services/ProductFileService.cs
@@ -0,0 +1,15 @@
+﻿using JsonMvcApp.Models;
+using System.Text.Json;
+
+namespace JsonMvcApp.Services
+{
+    public class ProductFileService
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment; // Окружение приложения
+        private readonly string _filePath; // Путь к JSON-файлу
+
+
+
+
+    }
+}
