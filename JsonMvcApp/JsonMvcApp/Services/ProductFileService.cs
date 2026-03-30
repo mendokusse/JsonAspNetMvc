647d25169d43c5579b909187003f9f8695dd7f4e
mendokusse <anngnev@yandex.ru>
2026-03-30 11:13:09 +0300

    Add ProductFileService constructor

--- JsonMvcApp/JsonMvcApp/Services/ProductFileService.cs
+++ JsonMvcApp/JsonMvcApp/Services/ProductFileService.cs
@@ -8,7 +8,24 @@ namespace JsonMvcApp.Services
         private readonly IWebHostEnvironment _webHostEnvironment; // Окружение приложения
         private readonly string _filePath; // Путь к JSON-файлу
 
+        public ProductFileService(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
 
+            var dataFolder = Path.Combine(_webHostEnvironment.ContentRootPath, "Data");
+
+            if (!Directory.Exists(dataFolder))
+            {
+                Directory.CreateDirectory(dataFolder);
+            }
+
+            _filePath = Path.Combine(dataFolder, "products.json");
+
+            if (!File.Exists(_filePath))
+            {
+                File.WriteAllText(_filePath, "[]");
+            }
+        }
 
 
     }
