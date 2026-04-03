1c924a0a3612065ce64903e62440068276d69dab
mendokusse <anngnev@yandex.ru>
2026-04-03 15:12:24 +0300

    Add edit page

---
+++ JsonMvcApp/JsonMvcApp/Pages/Products/Delete.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using JsonMvcApp.Models;
+using JsonMvcApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JsonMvcApp.Pages.Products
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ProductFileService _fileService;
+
+        [BindProperty]
+        public Product Product { get; set; } = new();
+
+        public DeleteModel(ProductFileService fileService)
+        {
+            _fileService = fileService;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            var product = _fileService.GetById(id);
+
+            if (product == null) return NotFound();
+
+            Product = product;
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            var isDeleted = _fileService.Delete(Product.Id);
+
+            if (!isDeleted) return NotFound();
+
+            return RedirectToPage("/Products/Index");
+        }
+    }
+}
--- JsonMvcApp/JsonMvcApp/Pages/Products/Edit.cshtml.cs
+++ JsonMvcApp/JsonMvcApp/Pages/Products/Edit.cshtml.cs
@@ -2,6 +2,7 @@
 using JsonMvcApp.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
 
 namespace JsonMvcApp.Pages.Products
 {
@@ -12,6 +13,44 @@ namespace JsonMvcApp.Pages.Products
         [BindProperty]
         public Product Product { get; set; } = new();
 
+        public EditModel(ProductFileService fileService)
+        {
+            _fileService = fileService;
+        }
 
+        public IActionResult OnGet(int id)
+        {
+            var product = _fileService.GetById(id);
+
+            if (product == null) return NotFound();
+
+            Product = product;
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (string.IsNullOrEmpty(Product.Name))
+            {
+                ModelState.AddModelError("Product.Name", "Название обязательно");
+            }
+
+            if (Product.Price <= 0)
+            {
+                ModelState.AddModelError("Product.Price", "Цена должна быть больше 0");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var isUpdated = _fileService.Update(Product);
+
+            if (!isUpdated) return NotFound();
+
+            return RedirectToPage("/Products/Index");
+        }
     }
 }
