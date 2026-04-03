135d53ced619a02ef4edd872bfb81f68be83b9bc
mendokusse <anngnev@yandex.ru>
2026-04-03 10:29:01 +0300

    Add details page and edit model

---
+++ JsonMvcApp/JsonMvcApp/Pages/Products/Details.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using JsonMvcApp.Services;
+using JsonMvcApp.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JsonMvcApp.Pages.Products
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ProductFileService _fileService;
+
+        public Product? Product { get; set; }
+
+        public DetailsModel(ProductFileService fileService)
+        {
+            _fileService = fileService;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            Product = _fileService.GetById(id);
+
+            if (Product == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+    }
+}
---
+++ JsonMvcApp/JsonMvcApp/Pages/Products/Edit.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using JsonMvcApp.Models;
+using JsonMvcApp.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JsonMvcApp.Pages.Products
+{
+    public class EditModel : PageModel
+    {
+        private readonly ProductFileService _fileService;
+
+        [BindProperty]
+        public Product Product { get; set; } = new();
+
+
+    }
+}
