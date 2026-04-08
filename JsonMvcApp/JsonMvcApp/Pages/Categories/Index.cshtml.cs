fc5e1d3bc5ede92d686a309fc0eb54385e909ad4
mendokusse <anngnev@yandex.ru>
2026-04-08 10:45:23 +0300

    Add categories index page

---
+++ JsonMvcApp/JsonMvcApp/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using JsonMvcApp.Data;
+using JsonMvcApp.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace JsonMvcApp.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AppDbContext _dbContext;
+
+        public List<Category> Categories { get; set; } = new();
+
+        public IndexModel(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task OnGet()
+        {
+            Categories = await _dbContext.Categories.ToListAsync();
+        }
+    }
+}
